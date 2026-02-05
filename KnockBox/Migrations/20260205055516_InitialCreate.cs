b87503572f4ceb047bdb12484da2530a5030d03e
jcub1011 <gitkraken+jcub1011@users.noreply.github.com>
2026-02-05 00:59:19 -0500

    Created initial migration.

---
+++ KnockBox/Migrations/20260205055516_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace KnockBox.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "TestEntity",
+                schema: "public",
+                columns: table => new
+                {
+                    TestModalId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TestData = table.Column<string>(type: "character varying(16)", maxLength: 16, nullable: false),
+                    TestDate = table.Column<DateTime>(type: "timestamp(0) with time zone", precision: 0, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestEntity", x => x.TestModalId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestEntity",
+                schema: "public");
+        }
+    }
+}
