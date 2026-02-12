21621ab6261434d60b65371f12c8dcdc88da7517
jcub1011 <gitkraken+jcub1011@users.noreply.github.com>
2026-02-11 22:47:45 -0500

    Working on game lobby implementation.

---
+++ KnockBox/Services/Logic/Games/Lobbies/IGameLobbyService.cs
@@ -0,0 +1,54 @@
+﻿using KnockBox.Services.Navigation.Games;
+
+namespace KnockBox.Services.Logic.Games.Lobbies
+{
+    /// <summary>
+    /// Represents a game lobby with a unique room code and an optional game type.
+    /// </summary>
+    /// <param name="RoomCode">The unique code identifying the game lobby. Cannot be null or empty.</param>
+    /// <param name="GameType">The type of game associated with the lobby, or null if not specified.</param>
+    public record class GameLobby(string RoomCode, GameType? GameType);
+
+    public record class LobbyFilter(GameType? Type);
+
+    public interface IGameLobbyService
+    {
+        /// <summary>
+        /// Gets all the active lobbies in this server.
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<List<GameLobby>> GetLobbiesAsync(CancellationToken ct = default);
+
+        /// <summary>
+        /// Gets all the active lobbies in this server matching the filter.
+        /// </summary>
+        /// <param name="filter"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<List<GameLobby>> GetLobbiesAsync(LobbyFilter filter, CancellationToken ct = default);
+
+        /// <summary>
+        /// Gets the lobby with the room code.
+        /// </summary>
+        /// <param name="roomCode"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<GameLobby?> GetLobbyAsync(string roomCode, CancellationToken ct = default);
+
+        /// <summary>
+        /// Creates a lobby of the specified type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task<GameLobby> CreateLobbyAsync(GameType type, CancellationToken ct = default);
+
+        /// <summary>
+        /// Ends the lobby with the room code.
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task EndLobbyAsync(string roomCode, CancellationToken ct = default);
+    }
+}
---
+++ KnockBox/Services/State/Games/Lobbies/GameLobby.cs
@@ -0,0 +1,90 @@
+﻿using KnockBox.Services.Navigation.Games;
+
+namespace KnockBox.Services.State.Games.Lobbies
+{
+    public abstract class GameLobby
+    {
+        private readonly Lock _lock = new();
+        private readonly ILogger _logger;
+
+        /// <summary>
+        /// Invoked when this lobby is closed.
+        /// </summary>
+        public event Func<GameLobby, Task>? LobbyClosed;
+
+        /// <summary>
+        /// The unique key for this lobby.
+        /// </summary>
+        public readonly string RoomCode;
+
+        /// <summary>
+        /// Null when a game isn't selected.
+        /// </summary>
+        public GameType? GameType
+        {
+            get
+            {
+                using var scope = _lock.EnterScope();
+                return field;
+            }
+            set
+            {
+                using var scope = _lock.EnterScope();
+                if (value == field) return;
+                field = value;
+
+            }
+        }
+
+        private readonly List<Guid> _connectedUsers;
+        public IReadOnlyList<Guid> ConnectedUsers
+        {
+            get 
+            {
+                using var scope = _lock.EnterScope();
+                return _connectedUsers.ToArray();
+            }
+        }
+
+        public GameLobby(string roomCode, ILogger logger)
+        {
+            RoomCode = roomCode;
+            GameType = null;
+            _logger = logger;
+            _connectedUsers = [];
+        }
+
+        protected async Task NotifyLobbyClosureAsync()
+        {
+            async Task NotifyHandlerAsync(Func<GameLobby, Task> handler)
+            {
+                try
+                {
+                    await handler.Invoke(this);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error notifying subscriber of lobby closure.");
+                }
+            }
+
+            var tasks = LobbyClosed?.GetInvocationList()
+                .Cast<Func<GameLobby, Task>>()
+                .Select(NotifyHandlerAsync) ?? [];
+
+            await Task.WhenAll(tasks);
+        }
+
+        public void RegisterUser(Guid user)
+        {
+            using var scope = _lock.EnterScope();
+            _connectedUsers.Add(user);
+        }
+
+        public void UnregisterUser(Guid user)
+        {
+            using var scope = _lock.EnterScope();
+            _connectedUsers.Remove(user);
+        }
+    }
+}
