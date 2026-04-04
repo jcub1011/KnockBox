0591a8349c0459b40f86a55aee7b52e6783507c8
jcub1011 <gitkraken+jcub1011@users.noreply.github.com>
2026-04-04 14:55:34 -0400

    Implemented number and operator cards.

--- KnockBox.Operator/Models/Card.cs
+++ KnockBox.Operator/Models/Card.cs
@@ -47,23 +47,97 @@ public interface IBlockableCard
     IEnumerable<Card> GetPotentialReactionCards(OperatorPlayerState playerState);
 }
 
-public class Card
+public abstract class Card
 {
     public Guid Id { get; init; } = Guid.NewGuid();
+
+    /// <summary>
+    /// The icon for this card.
+    /// </summary>
+    /// <returns></returns>
+    public abstract string CardIcon();
+
+    /// <summary>
+    /// The tooltip name of this card.
+    /// </summary>
+    /// <returns></returns>
+    public abstract string TooltipName();
+
+    /// <summary>
+    /// The tooltip description of this card.
+    /// </summary>
+    /// <returns></returns>
+    public abstract string TooltipDescription();
 }
 
-public class NumberCard(decimal numberValue = 0m)
+public class NumberCard(decimal numberValue = 0m) : Card
 {
     public decimal NumberValue { get; init; } = numberValue;
+
+    public override string CardIcon()
+        => $"{NumberValue:N0}";
+
+    public override string TooltipName()
+        => $"Number {NumberValue:N0}";
+
+    public override string TooltipDescription()
+        => "Play number cards to modify scores. Stack multiple numbers to form larger values (e.g. 3 + 7 = 37).";
 }
 
-public abstract class OperatorCard(CardOperator operatorValue = CardOperator.None) 
+public class OperatorCard(CardOperator operatorValue = CardOperator.None) 
     : Card, ITargetableCard, IBlockableCard
 {
     public CardOperator OperatorValue { get; init; } = operatorValue;
 
-    public abstract IEnumerable<Card> GetPotentialReactionCards(OperatorPlayerState playerState);
-    public abstract IEnumerable<OperatorPlayerState> GetPotentialTargets(OperatorGameContext context);
+    public override string CardIcon()
+    {
+        return OperatorValue switch
+        {
+            CardOperator.Add => "+",
+            CardOperator.Subtract => "-",
+            CardOperator.Multiply => "\u00d7",
+            CardOperator.Divide => "\u00f7",
+            _ => "?"
+        };
+    }
+
+    public override string TooltipName()
+    {
+        return OperatorValue switch
+        {
+            CardOperator.Add => "Add (+)",
+            CardOperator.Subtract => "Subtract (-)",
+            CardOperator.Multiply => "Multiply (\u00d7)",
+            CardOperator.Divide => "Divide (\u00f7)",
+            _ => "Unknown Operator"
+        };
+    }
+
+    public override string TooltipDescription()
+    {
+        return OperatorValue switch
+        {
+            CardOperator.Add => "Sets a player's active operator to Add. Future number cards will be added to their score.",
+            CardOperator.Subtract => "Sets a player's active operator to Subtract. Future number cards will be subtracted from their score.",
+            CardOperator.Multiply => "Sets a player's active operator to Multiply. Future number cards will multiply their score.",
+            CardOperator.Divide => "Sets a player's active operator to Divide. Future number cards will divide their score.",
+            _ => "Changes a player's active operator."
+        };
+    }
+
+    public IEnumerable<Card> GetPotentialReactionCards(OperatorPlayerState playerState)
+    {
+        // Only shield cards can block operators
+        return playerState.Hand.Where((card) 
+            => card is ActionCard action && action.ActionValue == CardAction.Shield);
+    }
+
+    public IEnumerable<OperatorPlayerState> GetPotentialTargets(OperatorGameContext context)
+    {
+        // Can't replace active operator if operator are the same type
+        return context.GamePlayers.Values.Where((player)
+            => player.ActiveOperator != OperatorValue);
+    }
 }
 
 public abstract class ActionCard(CardAction actionValue = CardAction.None) 
